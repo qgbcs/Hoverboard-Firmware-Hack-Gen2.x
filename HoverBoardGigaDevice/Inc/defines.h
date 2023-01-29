aa6e8b6f0517b41ac20e3d5bbdb7d7d25356da82
Robo Durden <roland@robosoft.de>
2023-01-29 18:21:29 +0100

    Update defines.h

--- HoverBoardGigaDevice/Inc/defines.h
+++ HoverBoardGigaDevice/Inc/defines.h
@@ -4,5 +4,5 @@
 //#include "defines_2-1.h"		// https://github.com/krisstakos/Hoverboard-Firmware-Hack-Gen2.1
 	// unique: hall input pins A,B,C = A0,A1,B11
 
-#include "defines_2-2.h"		// robo
+#include "defines_2-2.h"		// https://github.com/RoboDurden/Hoverboard-Firmware-Hack-Gen2.x
 	// unique: hall input pins A,B,C = A13,B0,A5
